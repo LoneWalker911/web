b1b83c7a2bf52eb05fe38ad9fff16e883a7ede8d
Prashan99 <Prashanvimanthajagode99@gmail.com>
2021-01-03 19:28:13 +0530

    nav bar, public home,login page

--- index.php
+++ index.php
@@ -4,23 +4,21 @@
 <!DOCTYPE html>
 <html>
 <link rel="stylesheet" type="text/css" href="publicMap.css">
-<script src="publicMap.js"></script>
+<?php include 'navbar/nav.php'; ?>
+
+<div id="googleMap" ></div>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAI6bwkbJkNfAXK0kqSVi21V7Ll0CnUzOM&callback=mainMap"></script>
+
+
 
 <body>
 
 <div id="side-panel"></div>
 
-<div id="googleMap" ></div>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAI6bwkbJkNfAXK0kqSVi21V7Ll0CnUzOM&callback=mainMap">
-</script>
 
+</script>
 <script>
 marker(7.85072,81.65716,5);
-
-
-
-
-marker(7.95072,81.55716,"","<ul>HH<li>Huuu/ul>");
 </script>
 
 
